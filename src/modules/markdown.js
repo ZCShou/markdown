3d0b4e7f833090da8efec915cd0098e3f905688c
ZCShou <72115@163.com>
2026-01-24 20:26:20 +0800

    refactor: add toml highlight

--- src/modules/markdown.js
+++ src/modules/markdown.js
@@ -34,6 +34,7 @@ import 'prismjs/components/prism-sql';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-toml';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-typescript';
